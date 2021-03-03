4b8642a3d93e99d2ee5e28ed32bbc021a339a99f
kolisergej <kolisergej@yandex.ru>
2021-03-03 20:52:26 +0300

    upd

--- main.cpp
+++ main.cpp
@@ -1,20 +1,9 @@
-// Main steps:
-// 1. if `s` starts with one of the closed brackets -> find last of its continuous sequence
-// 2. main tracked vars: 
-// - result: final answer
-// - current: input substring, tracked in the current moment
-// - stack: aux structure for valid brackets tracking
-// 3. Opened brackets push to `current` and `stack`, a-z push to `current` only, when meet closed brackets than:
-// - if stack empty: bring longest string
-// - if closed bracket not valid via opened one -> find out `current` valid substring and bring longest one
-// - else (closed bracket == stack top): just push to current
-
 #include <cassert>
+#include <iostream>
+#include <stack>
 #include <string>
 #include <unordered_set>
 #include <unordered_map>
-#include <stack>
-#include <iostream>
 
 using namespace std;
 
@@ -26,61 +15,7 @@ const unordered_map<char, char> kDict = {
 };
 const unordered_set<char> kValues{'}', ')', ']'};
 
-void process_current(string& result, string& current, stack<char>& st) {
-    size_t idx{0};
-    size_t wrong_idx{0};
-    const auto stack_size = st.size();
-    while (idx < current.size() && wrong_idx < stack_size) {
-        if (kDict.count(current[idx])) {
-            ++wrong_idx;
-            ++idx;
-            st.pop();
-        } else {
-            ++idx;
-        }
-    }
-    if (current.size()-idx > result.size()) {
-        result = current.substr(idx);
-    }
-    cout << result << ", " << idx << ", " << current << endl;
-    current.clear();
-}
-
-string process(const string& input) {
-    string result;
-    string current; 
-    stack<char> st;
-    for (const auto ch: input) {
-        if (kDict.count(ch)) {
-            st.push(ch);
-            current += ch;
-        } else if (kValues.count(ch)) {
-            if (st.empty()) {
-                if (current.size() > result.size()) {
-                    result = current;
-                }
-                current.clear();
-                continue;
-            }
-
-            const auto top = st.top();
-            if (kDict.at(top) != ch) {
-                process_current(result, current, st);
-            } else {
-                current += ch;
-                st.pop();
-            }
-        } else {
-            current += ch;
-        }
-    }
-    if (!current.empty()) {
-        process_current(result, current, st);
-    }
-    return result;
-}
-
-bool check_infinite(const std::string& input) {
+bool check_infinite(const string& input) {
     size_t first_open_idx = 0;
     while (first_open_idx < input.size() && kValues.count(input[first_open_idx])) {
         ++first_open_idx;
@@ -115,14 +50,92 @@ bool check_infinite(const std::string& input) {
     return st.empty();
 }
 
+string process_letters_only(const string& input) {
+    string result;
+    int start{-1};
+    size_t current_length{0};
+    for (size_t i = 0; i < input.size(); i++) {
+        const auto ch = input[i];
+        if (kDict.count(ch) || kValues.count(ch)) {
+            if (start != -1 && current_length > result.size()) {
+                result = input.substr(start, current_length);
+            }
+            current_length = 0;
+            start = -1;
+            continue;
+        }
+        if (start == -1) {
+            start = i;
+        }
+        ++current_length;
+    }
+    if (start != -1 && current_length > result.size()) {
+        result = input.substr(start, current_length);
+    }
+    return result;
+}
+
+string process_with_parrs(const string& input) {
+    int start{-1};
+    int end{-1};
+    stack<pair<char, int>> st;
+    for (int i = 0; i < input.size(); ++i) {
+        const auto ch = input[i];
+        if (kDict.count(ch)) {
+            st.push(make_pair(ch, i));
+        } else if (kValues.count(ch)) {
+            if (st.empty()) {
+                continue;
+            }
+            const auto top = st.top();
+            if (kDict.count(top.first) && kDict.at(top.first) == ch) {
+                st.pop();
+                int aux = 0;
+                while (!st.empty()) {
+                    if (kValues.count(st.top().first) || kDict.count(st.top().first)) {
+                        break;
+                    }
+                    ++aux;
+                    st.pop();
+                }
+                if (end == top.second - aux) {
+                    end = i+1;
+                } else if (top.second < start) {
+                    start = top.second;
+                    end = i+1;
+                } else if (end - start < (i - top.second) + aux) {
+                    start = top.second-aux;
+                    end = i+1;
+                }
+            } else {
+                stack<pair<char, int>>().swap(st);
+            }
+        } else if (!st.empty()) {
+            auto top = st.top();
+            if (kValues.count(top.first) || !kDict.count(top.first)) {
+                st.push(make_pair(ch, i));
+            }
+        } else {
+            st.push(make_pair(ch, i));
+        }
+        // cout << start << ", " << end << endl;
+    }
+    if (start != -1 && end != -1) {
+        return input.substr(start, end-start);
+    }
+    return "";
+}
+
 string run(const string& input) {
     if (check_infinite(input)) {
         return kInfinite;
     }
     
     const string double_input = input + input;
-    const string result = process(double_input);
-    cout << "result: " << result << endl;
+    const string letters_only_max = process_letters_only(double_input);
+    const string max_with_parrs = process_with_parrs(double_input);
+    const string result = (letters_only_max.size() > max_with_parrs.size()) ? letters_only_max : max_with_parrs;
+    // cout << "result: " << result << endl;
     return result;
 }
 
@@ -134,21 +147,45 @@ int main() {
     // assert(run("]h({hdb}b)[") == kInfinite);
     // assert(run("[[b[aa]]]") == kInfinite);
     // assert(run("]][[b[aa]") == kInfinite);
+    assert(run("aaaa)((aaa)") == kInfinite);
 
-    // assert(run("]})") == "");
-    // assert(run("())") == "()");
-    // assert(run("((aa)") == "(aa)");
-    // assert(run("[[]") == "[]");
-    // assert(run("[[])") == "[]");
-    // assert(run("}](){") == "(){}");
-    // assert(run("aabbcc)") == "aabbcc");
-    // assert(run("(aabbcc") == "aabbcc");
-    // assert(run("sh(dh)}") == "sh(dh)");
+    assert(run("]})") == "");
+    assert(run("())") == "()");
+    assert(run("((aa)") == "(aa)");
+    assert(run("[[]") == "[]");
+    assert(run("[[])") == "[]");
+    assert(run("}](){") == "(){}");
 
-    // assert(run("[[b[aa]]") == "[b[aa]]");
-    // assert(run("[[b[aa]]]]") == "[[b[aa]]]");
-    // assert(run("[[b[aa]]}") == "[b[aa]]");
-    // assert(run("]h({hh(b})b)[") == "hh");
+    assert(run("aa)((aaaaa") == "(aaaaaaa)");
+    assert(run("((aaa)((aa)") == "(aaa)");
+    assert(run("((aaa)((aaa)") == "(aaa)");
+    assert(run("aaaaaaa)") == "aaaaaaa");
+    assert(run("(aaaaaaa") == "aaaaaaa");
+    assert(run("((aa)(aa)") == "(aa)(aa)");
+    assert(run("(aa(aa)a(aa)a))") == "(aa(aa)a(aa)a)");
+    assert(run("((aa(aa)a(aa)a)") == "(aa(aa)a(aa)a)");
+
+    assert(run("(aa)aa(aa))") == "(aa)aa(aa)");
+    assert(run("(aa(aa)aa(aa)))") == "(aa(aa)aa(aa))");
+    // run("]h({hhh(b})b)[");
+    // run("]h({h(bb})b)[");
+    // run("]h({hdd(b})b)[");
+
+    assert(run("((aa)((aa)") == "(aa)");
+    assert(run("((aa)(aa)((aa)") == "(aa)(aa)");
+    assert(run("(aa))(aa))") == "(aa)");
+    assert(run("(aba))(aa))") == "(aba)");
+    assert(run("(aa)(aa))") == "(aa)(aa)");
+    assert(run("((a{a})))") == "((a{a}))");
+
+    assert(run("aabbcc)") == "aabbcc");
+    assert(run("(aabbcc") == "aabbcc");
+    assert(run("sh(dh)}") == "sh(dh)");
+
+    assert(run("[[b[aa]]") == "[b[aa]]");
+    assert(run("[[b[aa]]]]") == "[[b[aa]]]");
+    assert(run("[[b[aa]]}") == "[b[aa]]");
+    // assert(run("]h({hhh(b})b)[") == "hhh");
     // assert(run("]h({h(bb})b)[") == "[]h");
     // assert(run("]h({hdd(b})b)[") == "hdd");
     return 0;
